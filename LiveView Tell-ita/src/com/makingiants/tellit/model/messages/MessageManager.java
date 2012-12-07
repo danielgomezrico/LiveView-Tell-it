3750c8e9b166735782adf776bb4f84ba7fa68681
Daniel Gomez <danielgomezrico@Daniels-MacBook-Pro.local>
2012-12-07 01:30:34 -0500

    First app commit

---
+++ LiveView Tell-ita/src/com/makingiants/tellit/model/messages/MessageManager.java
@@ -0,0 +1,95 @@
+package com.makingiants.tellit.model.messages;
+
+import android.content.Context;
+
+import com.makingiants.tellit.model.dao.MessagesDAO;
+
+/**
+ * Manage the messages list
+ */
+public class MessageManager {
+	
+	// ****************************************************************
+	// Attributes
+	// ****************************************************************
+	
+	private String[] messages;
+	private int actualMessage;
+	
+	// ****************************************************************
+	// Constructor
+	// ****************************************************************
+	
+	public MessageManager(Context context, int numberOfMessages) {
+		
+		this.actualMessage = 0;
+		this.messages = MessagesDAO.getMessages(context, numberOfMessages);
+	}
+	
+	// ****************************************************************
+	// Accessor methods
+	// ****************************************************************
+	
+	/**
+	 * Add a message to the list if there is no message in the indexed position
+	 * @param index must be > 0
+	 * @param message
+	 */
+	public void addMessage(int index, String message) {
+		messages[index] = message;
+	}
+	
+	/**
+	 * Get the actual message
+	 * @return
+	 */
+	public String getActualMessage() {
+		if (messages.length != 0) {
+			return messages[actualMessage];
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Get the next message and update the actual message with it
+	 */
+	public String getNextMessage() {
+		if (messages.length != 0) {
+			String message = null;
+			
+			do {
+				if (++actualMessage >= messages.length) {
+					actualMessage = 0;
+				}
+				message = messages[actualMessage];
+				
+			} while (message == null || message.equals(""));
+			
+			return messages[actualMessage];
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * Get the previous message and update the actual message with it
+	 */
+	public String getPreviousMessage() {
+		if (messages.length != 0) {
+			String message = null;
+			do {
+				if (--actualMessage < 0) {
+					actualMessage = messages.length - 1;
+				}
+				message = messages[actualMessage];
+				
+			} while (message == null || message.equals(""));
+			
+			return messages[actualMessage];
+		} else {
+			return null;
+		}
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# LiveView Tell-it Plugin [![Google Play](http://developer.android.com/images/brand/en_generic_rgb_wo_45.png)](https://play.google.com/store/apps/details?id=com.makingiants.answerit)
+
+<a href="https://play.google.com/store/apps/details?id=com.makingiants.answerit" alt="Download from Google Play">
+	<img src="http://dc201.4shared.com/img/4qS5ZZQo/0.4095983652860181/banner.png">
+</a>
+
+This is a plugin for the LiveView device
+(a "smartwatch" for Android).
+
+Sometimes you need to send messages to your favorites contacts...
+and that messages sometimes are like "I will go now", "I'm here, ¿where are you?".
+
+Send this messages without having to get your phone out!
+
+You can predefine your messages.
+
+Developed using Eclipse and Android Plugin
+
+
+## License
+
+* [Apache Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+
+## Contributing
+
+Please fork this repository and contribute back using pull requests.
+
+Any contributions, large or small, major features, bug fixes, additional
+language translations, unit/integration tests are welcomed and appreciated
+but will be thoroughly reviewed and discussed.
+
+
+## About
+	
++ MakinGIANTS
++ http://www.makingiants.com
++ here.makin.giants@gmail.com
